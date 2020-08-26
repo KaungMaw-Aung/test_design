e1457b13b1f284be516b1498fde575222585965c
KaungMawAung <kaungmawaung99@gmail.com>
2020-08-26 12:38:31 +0630

    testing design

---
+++ app/src/main/java/com/kaungmaw/collapsingtoolbarexperiment/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.kaungmaw.collapsingtoolbarexperiment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
